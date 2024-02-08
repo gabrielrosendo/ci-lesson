a6aa61fc10dfb41d80ef3ebf79ccf0978cac45a3
GABRIELROSENDO <GABRIELROSEND011@GMAIL.COM>
2024-02-07 21:04:59 -0700

    Initial Commit

---
+++ tests/test_script.py
@@ -0,0 +1,7 @@
+import subprocess
+import sys
+
+def test_script_output(capfd):
+    subprocess.run(["python3", './script.py'])
+    captured = capfd.readouterr()
+    assert captured.out == "Hello, World!\n"
